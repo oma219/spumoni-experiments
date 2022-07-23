2e4e3a434c22064ad510d717bfa00f56286bc9fb
oma219 <omaryfekry@gmail.com>
2022-07-23 08:21:36 -0400

    added initial version of exp8

---
+++ src/process_lengths_exp8.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+# Name: process_lengths_exp8.py
+# Description: Takes in a SPUMONI lengths file and computes report,
+#              and csv files in order to plot the data.
+# Date: July 22nd, 2022
+
+import os
+import sys
+import argparse
+
+def main(args):
+    """ process the lengths file and generate report and csvs """
+
+    # rank all the contigs by the longest length
+    with open(args.lengths_file, "r") as input_fd:
+        curr_seq = ""
+        avg_dict = {}
+        for i, line in enumerate(input_fd):
+            if i % 2 == 0:
+                curr_seq = line.strip()
+            else:
+                lengths = [int(x) for x in line.split()]
+                avg = sum(lengths)/len(lengths)
+                #max_val = max(lengths)
+                avg_dict[curr_seq] = avg
+
+    # sort them, and keep all the contigs with   
+    sorted_dict = dict(sorted(avg_dict.items(), key=lambda item: item[1]))
+    top_contigs = []
+    for key in list(sorted_dict)[::-1]:
+        if sorted_dict[key] >= 20:
+            top_contigs.append(key)
+    assert len(top_contigs) > 0, "assertion error: did not find any contigs with large average MS"
+
+    # write out a report containing each contig and max length
+    with open(args.output_dir + "assembly_length_report.txt", "w") as out_fd:
+        out_fd.write("{:30}{:20}\n".format("name:", "max length:"))
+        for name in list(sorted_dict)[::-1]:
+            out_fd.write("{:30}{:<20}\n".format(name, sorted_dict[name]))
+    
+    # write out a csv-file for each of the top-ten contigs
+    with open(args.lengths_file, "r") as input_fd:
+        curr_seq = ""
+        total_sampled = []
+        for i, line in enumerate(input_fd):
+            if i % 2 == 0:
+                curr_seq = line.strip()
+            else:
+                if curr_seq in top_contigs:
+                    lengths = [int(x) for x in line.split()]
+                    index = top_contigs.index(curr_seq)
+                    
+                    with open(args.output_dir + f"top_contig_{index}_lengths.csv", "w") as out_fd:
+                        for i, l in enumerate(lengths):
+                            out_fd.write(f"{curr_seq[1:]},{i},{l}\n")
+                else:
+                    lengths = [int(x) for x in line.split()]
+                    for i in range(0, len(lengths), 10000):
+                        total_sampled.append(lengths[i])
+    
+
+    # write out a csv-file for the "regular" contigs
+    with open(args.output_dir + "regular_contigs.csv", "w") as out_fd:
+        for length in total_sampled:
+            out_fd.write(f"regular,{length}\n")
+
+
+
+def parse_arguments():
+    """ Parse the command-line arguments """
+    parser = argparse.ArgumentParser(description="Take in a lengths file from exp8, and process it.")
+    parser.add_argument("-i", dest="lengths_file", help="path to lengths file", required=True)
+    parser.add_argument("-o", dest="output_dir", help="path to output directory", required=True)
+    args = parser.parse_args()
+    return args
+
+def check_arguments(args):
+    """ Verify the correctness of the arguments """
+    if not os.path.isfile(args.lengths_file):
+        print("Error: the input file is not valid.")
+        exit(1)
+    if not os.path.isdir(args.output_dir):
+        print("Error: the output directory is not valid")
+        exit(1)
+    else:
+        if args.output_dir[-1] != "/":
+            args.output_dir += "/"
+    
+if __name__ == "__main__":
+    args = parse_arguments()
+    check_arguments(args)
+    main(args)
---
+++ workflow/rules/exp_8.smk
@@ -0,0 +1,95 @@
+##################################################
+# Name: exp_8.smk
+# Description: Contains the workflow and methods
+#              needed for experiment 8.
+#
+# Date: July 20, 2022
+##################################################
+
+####################################################
+# Section 1: Helper functions needed for these
+#            experiment rules.
+####################################################
+
+def list_of_all_genomes_to_index_exp8(wildcards):
+    """
+    This method grabs all the files that we need for
+    our SPUMONI index to use for contamination detection.
+    """
+    input_files = []
+    for num in range(1, num_datasets_exp8):
+        for data_file in os.listdir(f"data/dataset_{num}"):
+            if data_file.endswith(".fna"):
+                input_files.append(f"{base_dir}/data/dataset_{num}/{data_file}")
+    return input_files
+
+def get_current_assembly_exp8(wildcards):
+    """
+    Returns the path to the current assembly that we want
+    to compute MS for against the database
+    """
+    input_files = []
+    for data_file in os.listdir(f"data/assembly_{wildcards.asm_num}"):
+        if data_file.endswith(".fna") or data_file.endswith(".fa"):
+            input_files.append(f"{base_dir}/data/assembly_{wildcards.asm_num}/{data_file}")
+    assert len(input_files) == 1, "assertion error: there should only be one assembly"
+    return input_files[0]
+
+####################################################
+# Section 2: Rules needed for this experiment type
+####################################################
+
+# Section 2.1: Build the SPUMONI index over genomes that are common sources of contamination
+
+rule build_full_reference_file_exp8:
+    input:
+        list_of_all_genomes_to_index_exp8
+    output:
+        "exp8_full_reference/full_ref.fa"
+    shell:
+        """
+        cat {input} > {output}
+        """
+
+rule build_spumoni_index_of_contamination_exp8:
+    input:
+        "exp8_full_reference/full_ref.fa"
+    output:
+        "exp8_index/spumoni_full_ref.fa",
+        "exp8_index/spumoni_full_ref.fa.thrbv.ms",
+        "exp8_index/spumoni_full_ref.fa.msnulldb"
+    shell:
+        """
+        cp {input} exp8_index/full_ref.fa
+        spumoni build -r exp8_index/full_ref.fa -n -M -P
+        """
+
+
+# Section 2.2: Compute MS for each assembly against the contamination database
+
+rule run_spumoni_against_database_exp8:
+    input:
+        get_current_assembly_exp8,
+        "exp8_index/spumoni_full_ref.fa"
+    output:
+        "exp8_results/assembly_{asm_num}/input_assembly.fa",
+        "exp8_results/assembly_{asm_num}/input_assembly.fa.lengths"
+    shell:
+        """
+        cp {input[0]} {output[0]}
+        spumoni run -t 16 -r {input[1]} -p {output[0]} -M -n
+        """
+
+# Section 2.3: Generate analysis/csv files to use for plotting
+
+rule analyze_spumoni_results_against_database_exp8:
+    input:
+        "exp8_results/assembly_{asm_num}/input_assembly.fa.lengths"
+    output:
+        "exp8_analysis/assembly_{asm_num}/assembly_length_report.txt"
+    shell:
+        """
+        python3 {repo_dir}/src/process_lengths_exp8.py -i {input} \
+        -o exp8_analysis/assembly_{wildcards.asm_num}/
+        """
+
